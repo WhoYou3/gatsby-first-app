245f0604e776e31412933e3975184f255418fc5d
Jan Irzyk <janirzyk333@gmail.com>
2023-03-07 12:10:33 +0100

    gatsby tutorial

--- gatsby-first-app/src/components/recipeslist/RecipesList.js
+++ gatsby-first-app/src/components/recipeslist/RecipesList.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { Link } from "gatsby";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import slugify from "slugify";
 
 const RecipesList = ({ recipes = [] }) => {
   const recipesList = recipes.map((recipe) => {
     const { id, title, image, prepTime, cookTime } = recipe;
     const pathToImage = getImage(image);
+    const slug = slugify(title, { lower: true });
 
     return (
-      <Link key={id} className="recipe" to={`/${title}`}>
+      <Link key={id} className="recipe" to={`/${slug}`}>
         <GatsbyImage
           image={pathToImage}
           className="recipe-img"
--- gatsby-first-app/src/pages/tags.js
+++ gatsby-first-app/src/pages/tags.js
@@ -1,12 +1,39 @@
 import React from "react";
 import { Layout } from "../components/index";
+import { graphql, Link } from "gatsby";
+import setupTags from "../utils/setupTags";
 
-const Tags = () => {
+const Tags = ({ data }) => {
+  const newTags = setupTags(data.allContentfulRecipe.nodes);
   return (
     <Layout>
-      <div>Tags</div>
+      <main className="page">
+        <section className="tags-page">
+          {newTags.map((tag, index) => {
+            const [text, value] = tag;
+            return (
+              <Link to={`${text}`} className="tag">
+                <h5>{text}</h5>
+                <p>{value} recipe</p>
+              </Link>
+            );
+          })}
+        </section>
+      </main>
     </Layout>
   );
 };
+
+export const query = graphql`
+  query {
+    allContentfulRecipe {
+      nodes {
+        content {
+          tags
+        }
+      }
+    }
+  }
+`;
 
 export default Tags;
